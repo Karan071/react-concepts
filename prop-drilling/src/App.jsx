0cf5332c13195bb4d7bf2fdc261d512cf0945f10
Karan <kc2532002@gmail.com>
2024-08-24 11:21:51 +0530

    added

--- prop-drilling/src/App.jsx
+++ prop-drilling/src/App.jsx
@@ -1,6 +1,6 @@
 // import React, { useContext, useState } from "react";
 import { countAtom } from "./Store/atoms/count";
-import { RecoilRoot,useRecoilValue, useRecoilState } from 'recoil';
+import { RecoilRoot,useRecoilValue, useRecoilState,useSetRecoilState } from 'recoil';
 
 const App = () => {
   //wrap anyone that wants to use the teleported value inside a provider
@@ -17,6 +17,7 @@ function Count() {
     <div>
       <CountRenderer />
       <Buttons />
+      <TextArea/>
     </div>
   );
 }
@@ -25,18 +26,40 @@ function CountRenderer() {
   const count = useRecoilValue(countAtom);
   return (
     <div>
-      <b>{count}</b>
+      {count}
     </div>
   );
 }
 
+function TextArea(){
+  const count = useRecoilValue(countAtom);
+  return(
+    <div>
+      {count % 2==0 ? "it is even" : ""}
+    </div>
+  )
+}
+
+
 // Component button
 function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+  // here we dont exactly need count
+  // const [count, setCount] = useRecoilState(countAtom);
+  const setCount  = useSetRecoilState(countAtom); // now the buttons is also not gettin g re-rendered
+  console.log("Re-rendered")
+
+
+  //three ways to update the setCount 
+  //setCount(count + 1);
+  //setCount(prev => prev + 1)
+  //setCount(function(c)
+  // return c + 1;  
+// )
+  console.log("re-render-button")
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increase the count</button>
-      <button onClick={() => setCount(count - 1)}> Decrease the count </button>
+      <button onClick={() => setCount(count => count + 1)}>Increase the count</button>
+      <button onClick={() => setCount(count => count - 1)}> Decrease the count </button>
     </div>
   );
 }
