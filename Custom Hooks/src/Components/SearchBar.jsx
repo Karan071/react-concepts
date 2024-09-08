779cd0c4a4ccc5c871b892fe4106d64362ec834e
Karan <kc2532002@gmail.com>
2024-09-09 02:36:10 +0530

    debounced

--- Custom Hooks/src/Components/SearchBar.jsx
+++ Custom Hooks/src/Components/SearchBar.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useDebounce from '../Hooks/useDebounce';
 
 const SearchBar = () => {
     const [inputValue,setInputValue] = useState('');
     const debouncedValue = useDebounce(inputValue,500); //500 milli-secs debounce delay
+
+    //after delay of 500ms we can fetch
+    useEffect(()=>{
+        fetch("")
+    },[debouncedValue])
 
 
   return (
